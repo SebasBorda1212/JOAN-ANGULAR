2f6117b99a19bc9757734a70c19accf8ee319c5e
SebasBorda1212 <johansebastianborda@gmail.com>
2026-02-11 11:58:45 -0500

    primer

---
+++ PISTONS/src/app/app.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './shared/header/header.component';
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, HeaderComponent],
+  templateUrl: './app.html',
+})
+export class AppComponent {}
