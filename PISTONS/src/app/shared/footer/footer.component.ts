afefaa9be533717bb03248d6b3bfad87ed403787
SebasBorda1212 <johansebastianborda@gmail.com>
2026-02-12 15:52:13 -0500

    Pagina_proto

---
+++ PISTONS/src/app/shared/footer/footer.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  standalone: true,
+  template: `
+    <div style="background:red; color:white; padding:20px; text-align:center;">
+      SOY EL FOOTER 🔥
+    </div>
+  `
+})
+export class FooterComponent {}
