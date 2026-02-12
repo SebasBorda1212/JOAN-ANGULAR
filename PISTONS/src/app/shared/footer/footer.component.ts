c6fb2880dc1922d36e0239cf5840902033333550
SebasBorda1212 <johansebastianborda@gmail.com>
2026-02-12 16:19:21 -0500

    cambiook

--- PISTONS/src/app/shared/footer/footer.component.ts
+++ PISTONS/src/app/shared/footer/footer.component.ts
@@ -3,10 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-footer',
   standalone: true,
-  template: `
-    <div style="background:red; color:white; padding:20px; text-align:center;">
-      SOY EL FOOTER 🔥
-    </div>
-  `
+  templateUrl: './footer.html',
+  styleUrl: './footer.css'
 })
-export class FooterComponent {}
+export class FooterComponent {
+  currentYear: number = new Date().getFullYear();
+}
